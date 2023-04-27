5b486ff614d03db2be5370ca3756388f1561dd64
Bilou <pierrepillot0110@gmail.com>
2023-04-27 13:49:07 +0200

    up

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,26 +1,23 @@
 const registerServiceWorker = async () => {
-    if ('serviceWorker' in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register(
-                './service-worker.js',
-            {
-                scope: './',
-            }
-            );
-            if (registration.installing) {
-                console.log('Service worker installing');
-            } else if (registration.waiting) {
-                console.log('Service worker installed');
-            } else if (registration.active) {
-                console.log('Service worker active');
-            }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
+    if ("serviceWorker" in navigator) {
+      try {
+        const registration = await navigator.serviceWorker.register("./service-worker.js", {
+          scope: "./",
+        });
+        if (registration.installing) {
+          console.log("Installation du service worker en cours");
+        } else if (registration.waiting) {
+          console.log("Service worker installé");
+        } else if (registration.active) {
+          console.log("Service worker actif");
         }
+      } catch (error) {
+        console.error(`L'enregistrement a échoué : ${error}`);
+      }
     }
-};
-
-registerServiceWorker()
+  };
+  
+  registerServiceWorker();
 
 
 
